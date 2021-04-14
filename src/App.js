30fcc2d8a23c53fbbac418175d629722afcd8453
ayushjha119 <ayushjha81083@gmail.com>
2021-04-14 21:05:48 +0530

    initial navbar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import MyNavbar from "./components/my-navbar/mynavbar.components";
+
+import './App.css';
+
+const App = () =>  {
+  return (
+    <div >
+    <MyNavbar/>
+     My portfolio
+    </div>
+  );
+}
+
+export default App;
